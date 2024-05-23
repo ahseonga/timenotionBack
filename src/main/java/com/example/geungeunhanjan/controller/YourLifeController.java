be04c187d49bbadf0a40851527fee5c144d26cc1
taemin1997 <taemin9709@gmail.com>
2024-05-23 21:43:08 +0900

    controller

---
+++ src/main/java/com/example/geungeunhanjan/controller/YourLifeController.java
@@ -0,0 +1,18 @@
+package com.example.geungeunhanjan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class YourLifeController {
+
+//    @Controller
+//    @RequestMapping("/yourLife")
+//    public class HomeController {
+//
+//        @GetMapping("/yourLife")
+//        public String yourLife() {
+//            return "yourLife/yourLife";
+//        }
+//    }
+}
