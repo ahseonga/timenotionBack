4ebbddb0842707f228c9c0c65baa4694bfdbf414
mdongjoo <gooddongjoo@naver.com>
2024-06-02 16:31:26 +0900

    5차커밋

---
+++ src/main/java/com/example/geungeunhanjan/service/Mail/EmailService.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace(); // 또는 로깅
+        }
+    }
+}
